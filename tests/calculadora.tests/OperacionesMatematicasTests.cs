4c18b2712a94ec0c59393fb970320cbe88e201e6
Minnette <minnete.dh@gmail.com>
2026-03-11 13:11:04 -0600

    modificar

---
+++ tests/calculadora.tests/OperacionesMatematicasTests.cs
@@ -0,0 +1,49 @@
+using Calculadora;
+
+namespace Calculadora.Tests;
+
+public class OperacionesMatematicasTests{
+    private readonly OperacionesMatematicas _operacionesMatematicas = new();
+
+
+    [Fact]
+    public void Sumar_DosPositivos_RetornaSuma()
+    {
+        // Arrange
+        int a = 5, b = 10;
+
+        // Act
+        int resultado = _operacionesMatematicas.Sumar(a, b);
+
+        // Assert
+        Assert.Equal(15, resultado);
+    }
+
+
+    [Fact]
+    public void Sumar_NegativoMasPositivo_RetornaValorCorrecto()
+    {
+        // Arrange
+        int a = -5, b = 10;
+
+        // Act
+        int resultado = _operacionesMatematicas.Sumar(a, b);
+
+        // Assert
+        Assert.Equal(5, resultado);
+    }
+
+    [Fact]
+    public void Sumar_CeroMasCero_RetornaCero()
+    {
+        // Arrange
+        int a = 0, b = 0;
+
+        // Act
+        int resultado = _operacionesMatematicas.Sumar(a, b);
+
+        // Assert
+        Assert.Equal(0, resultado);
+    }
+}
+
